817807a8b84775f2112e7a53727efb7b61fce66d
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-05-05 17:15:48 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -1,13 +1,9 @@
 package com.devsuperior.dsvendas.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsvendas.entities.Seller;
 
 public interface SellerRepository extends JpaRepository<Seller, Long>{
-
-	List<Seller> findAll();
 
 }
