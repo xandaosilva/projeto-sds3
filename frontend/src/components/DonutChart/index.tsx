169962f52eb2efefab39297376c0478d104790fd
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-05-09 01:47:51 -0300

    DonutChart problem

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -21,7 +21,7 @@ const DonutChart = () => {
             setChartData({ labels: myLabels, series: mySeries });
             console.log(chartData)
         })
-    }, []);
+    },[]);
 
     const options = {
         legend: {
